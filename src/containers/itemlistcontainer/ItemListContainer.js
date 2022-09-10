5328cd2d818b17af0fb3bdeb213457ed6c291b91
Roberth Lopez <alejandroberth@gmail.com>
2022-09-10 01:58:59 -0500

    Contador con boton

--- src/containers/itemlistcontainer/ItemListContainer.js
+++ src/containers/itemlistcontainer/ItemListContainer.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import ItemCount from '../../components/itemcount/ItemCount'
 
 const ItemListContainer = ({greeting}) => {
+
+  const onAdd = (qty) => {
+    console.log(qty)
+  }
+  
   return (
+    <>
     <div className='flex place-content-center'>{greeting}</div>
+    <ItemCount stock="6" initial="1" onAdd={onAdd}/>
+    </>
   )
 }
 
